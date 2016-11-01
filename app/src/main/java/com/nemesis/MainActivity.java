8628f28d6be67d33fd57e7cdeebb5e89e4fdec44
Fourp065 <fourp065@gmail.com>
2016-11-01 20:00:04 +0700

    Добавил: в MainActivity OnCreate(), сначала запускается activity_main.xml где есть кнопка newgame, добавил обработчик этой кнопки который запускает MainGamePanel

--- app/src/main/java/com/nemesis/MainActivity.java
+++ app/src/main/java/com/nemesis/MainActivity.java
@@ -1,39 +1,51 @@
 package com.nemesis;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-	
-	private static final String TAG = MainActivity.class.getSimpleName();
-	
+    /**
+     * Called when the activity is first created.
+     */
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // requesting to turn the title OFF
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        // making it full screen
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        // set our MainGamePanel as the View
-        setContentView(new MainGamePanel(this));
-        Log.d(TAG, "View added");
+
+        setContentView(R.layout.activity_main);
+        final Context context = this;
+
+        Button newGameButton = (Button) findViewById(R.id.newgamebutton);
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // making it full screen
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                // set our MainGamePanel as the View
+                setContentView(new MainGamePanel(context));
+            }
+        });
+
     }
 
-	@Override
-	protected void onDestroy() {
-		Log.d(TAG, "Destroying...");
-		super.onDestroy();
-	}
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "Destroying...");
+        super.onDestroy();
+    }
 
-	@Override
-	protected void onStop() {
-		Log.d(TAG, "Stopping...");
-		super.onStop();
-	}
-    
-    
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "Stopping...");
+        super.onStop();
+    }
 }
